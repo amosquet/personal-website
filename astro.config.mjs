cb19624f89481583b11832565191f33e36e4e816
amosquet <artusmosquet@gmail.com>
2026-02-16 08:20:50 -0500

    fixed JP language

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   i18n: {
-      locales: ["en", "fr", "jp"],
-      defaultLocale: "en",
-      // localeDetection: true,
-      fallback: {
-          fr : "en",
-          jp : "en"
-      },
-      routing: {
-          prefixDefaultLocale: true,
-          fallbackType: "rewrite"
-      }
+    locales: ["en", "fr", "jp"],
+    defaultLocale: "en",
+    // localeDetection: true,
+    fallback: {
+      fr: "en",
+      jp: "en",
+    },
+    routing: {
+      prefixDefaultLocale: true,
+      fallbackType: "rewrite",
+    },
   },
 
-  output: 'server',
+  output: "server",
 
   prefetch: {
-      prefetchAll: true,
-      defaultStrategy: 'viewport',
+    prefetchAll: true,
+    defaultStrategy: "viewport",
   },
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
