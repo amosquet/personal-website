981939fac672a3feacfc19aa65c6f48bdcf4fc2d
amosquet <artusmosquet@gmail.com>
2026-04-13 11:08:03 -0400

    should be good for basic about layout

--- src/i18n/uiStrings.ts
+++ src/i18n/uiStrings.ts
@@ -1,77 +1,75 @@
 import type { Locale } from "@/i18n/i18n";
 
 export interface TypeUIStrings {
-    [key: string]: {
-        [locale in Locale]: string | undefined;
-    };
+  [key: string]: {
+    [locale in Locale]: string | undefined;
+  };
 }
 
-
-
 export const uiStrings: TypeUIStrings = {
-    siteTitle: {
-        en: "Artus Mosquet",
-        fr: "Artus Mosquet",
-        jp: "モスケ アータス",
-    },
-    navHome: {
-        en: "Home",
-        fr: "Accueil",
-        jp: "主要", //fix?
-    },
-    navBlog: {
-        en: "Blog",
-        fr: "Blog",
-        jp: "ブログ",
-    },
-    navAbout: {
-        en: "About",
-        fr: "À propos",
-        jp: "約", //fix?
-    },
-    navContact: {
-        en: "Contact",
-        fr: "Contact",
-        jp: "連絡先", //fix 接触?
-    },
-    myCountry: {
-        en: "United States",
-        fr: "États-Unis",
-        jp: "アメリカ",
-    },
-    myOccupation: {
-        en: "Full-Time ECE Student",
-        fr: "Étudiant en ECE à temps plein",
-        jp: "フルタイムのECE学生", //fix
-    },
-    footerProject: {
-        en: "Learn more about my projects on",
-        fr: "En savoir plus sur mes projets sur",
-        jp: "私のプロジェクトについては、", //fix?
-    },
-    footerSocial: {
-        en: "Follow me on",
-        fr: "Suivez-moi sur",
-        jp: "私をフォローしてください", //fix?
-    },
-    welcome: {
-        en: "Welcome!",
-        fr: "Bienvenue!",
-        jp: "ようこそ！", //fix?
-    },
-    publishDate: {
-        en: "Published on:",
-        fr: "Publié le:",
-        jp: "公開日:", //fix?
-    },
-    writtenBy: {
-        en: "Written by:",
-        fr: "Écrit par:",
-        jp: "著者:", //fix?
-    },
-    languages: {
-        en: "French and English",
-        fr: "Français et anglais",
-        jp: "フランス語と英語", //fix?
-    },
+  siteTitle: {
+    en: "Artus Mosquet",
+    fr: "Artus Mosquet",
+    jp: "モスケ アータス",
+  },
+  navHome: {
+    en: "Home",
+    fr: "Accueil",
+    jp: "主要", //fix?
+  },
+  navBlog: {
+    en: "Blog",
+    fr: "Blog",
+    jp: "ブログ",
+  },
+  navAbout: {
+    en: "About",
+    fr: "À propos",
+    jp: "約", //fix?
+  },
+  navContact: {
+    en: "Contact",
+    fr: "Contact",
+    jp: "連絡先", //fix 接触?
+  },
+  myCountry: {
+    en: "United States",
+    fr: "États-Unis",
+    jp: "アメリカ",
+  },
+  myOccupation: {
+    en: "Full-Time ECE Student",
+    fr: "Étudiant en ECE à temps plein",
+    jp: "電気工学専攻のフルタイム学生", //fix
+  },
+  footerProject: {
+    en: "Learn more about my projects on",
+    fr: "En savoir plus sur mes projets sur",
+    jp: "私のプロジェクトについては、", //fix?
+  },
+  footerSocial: {
+    en: "Follow me on",
+    fr: "Suivez-moi sur",
+    jp: "私をフォローしてください", //fix?
+  },
+  welcome: {
+    en: "Welcome!",
+    fr: "Bienvenue!",
+    jp: "ようこそ！", //fix?
+  },
+  publishDate: {
+    en: "Published on:",
+    fr: "Publié le:",
+    jp: "公開日:", //fix?
+  },
+  writtenBy: {
+    en: "Written by:",
+    fr: "Écrit par:",
+    jp: "著者:", //fix?
+  },
+  languages: {
+    en: "French and English",
+    fr: "Français et anglais",
+    jp: "フランス語と英語", //fix?
+  },
 };
