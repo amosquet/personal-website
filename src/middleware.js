4e259c9052aeab106000b72d9de9fb7df688f651
amosquet <artusmosquet@gmail.com>
2026-04-25 23:24:59 -0400

    hhhhh

--- src/middleware.js
+++
@@ -1,9 +0,0 @@
-// import { defineMiddleware } from "astro:middleware";
-// import { redirectToDefaultLocale } from "astro:i18n"; // function available with `manual` routing
-// export const onRequest = defineMiddleware(async (ctx, next) => {
-//   if (ctx.url.startsWith("/about")) {
-//     return next();
-//   } else {
-//     return redirectToDefaultLocale(302);
-//   }
-// })
