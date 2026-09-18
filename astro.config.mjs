ca3e45152b351053fbd5e8a12c51e571beaee3db
AlmondMan <github@amcloud.dev>
2026-09-18 15:35:20 -0400

    Astro is annoying me

--- astro.config.mjs
+++ astro.config.mjs
@@ -53,7 +53,7 @@ export default defineConfig({
     },
     routing: {
       prefixDefaultLocale: true,
-      redirectToDefaultLocale: true,
+      redirectToDefaultLocale: false,
       fallbackType: "rewrite",
     },
   },
